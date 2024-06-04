389c96c4e16ef268048b12e054acd79b3be504cf
Defaultik <defaultik.personal@gmail.com>
2024-06-04 11:27:30 +0300

    font fix; code optimization

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-import pygame, cv2
+import pygame
+import time
+import cv2
 
-from datetime import datetime
 from djitellopy import Tello
 
 
-def GetKey(key):
+def key_pressed(key):
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == getattr(pygame, 'K_{}'.format(key)):
+                return True
+    
+    return False
+
+
+def key_holded(key):
     for event in pygame.event.get():
         pass
 
@@ -14,112 +24,92 @@ def GetKey(key):
     return False
 
 
-def GetKeyOnce(key):
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == getattr(pygame, 'K_{}'.format(key)):
-                return True
-        else:
-            pass
-
-        return False
-
-
 class Drone:
-    def in_fly(self):
-        if self.drone.get_flight_time:
-            return True
-        
-        return False
-
-
     def __init__(self):
+        self.drone = Tello()
+
+        self.drone.connect()
+        self.send_rc_control = False
+        
+        self.drone.streamoff()
+        self.drone.streamon()
+
         pygame.init()
         pygame.display.set_caption("Drone Control")
 
         self.screen = pygame.display.set_mode([960, 720])
 
-        self.drone = Tello()
-
-        self.control = False
-        self.send_rc_control = False
-
     
+    def in_fly(self):
+        if self.drone.get_height():
+            return True
+        
+        return False
+
+
     def start(self):
-        self.drone.connect()
-
-        self.drone.streamoff()
-        self.drone.streamon()
-
         frame_read = self.drone.get_frame_read()
         while True:
-            frame = frame_read.frame
+            self.frame = frame_read.frame
 
-            cv2.putText(frame, str(self.drone.get_battery()) + "%", (10, 30), cv2.FONT_HERSHEY_COMPLEX, 0.7, (255, 255, 255), 2)
-            cv2.putText(frame, datetime.now().strftime("%H:%M:%S"), (10, 65), cv2.FONT_HERSHEY_COMPLEX, 0.7, (255, 255, 255), 2)
+            cv2.putText(self.frame, str(self.drone.get_battery()) + "%", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1, cv2.LINE_AA)
+            cv2.putText(self.frame, time.strftime("%H:%M:%S"), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1, cv2.LINE_AA)
 
-            if GetKey("HOME"):
-                print("Screenshot successful")
+            self.frame = pygame.surfarray.make_surface(self.frame)
+            self.frame = pygame.transform.rotate(self.frame, 90)
+            self.frame = pygame.transform.flip(self.frame, False, True)
+            
+            self.control()
 
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                cv2.imwrite(datetime.now().strftime("H%H.M%M.S%S") + ".jpg", frame)
-
-            frame = pygame.surfarray.make_surface(frame)
-            frame = pygame.transform.rotate(frame, 90)
-            frame = pygame.transform.flip(frame, False, True)
-
-            self.screen.blit(frame, (0, 0))
-
+            self.screen.blit(self.frame, (0, 0))
             pygame.time.Clock().tick(30)
             pygame.display.update()
 
-            self.controller()
+    
+    def control(self):
+        fb, lr, ud, cw = 0, 0, 0, 0
+        speed = 60
+
+        if key_pressed("SPACE"):
+            if self.in_fly():
+                self.drone.land()
+                self.send_rc_control = False
+            else:
+                self.drone.takeoff()
+                self.send_rc_control = True
+
+        if self.send_rc_control:
+            if key_holded("LSHIFT"):
+                speed += 40
+            else:
+                speed = 60
+
+            if key_holded("w"):
+                fb = speed
+
+            if key_holded("a"):
+                lr = -speed
+
+            if key_holded("s"):
+                fb = -speed
+
+            if key_holded("d"):
+                lr = speed
+
+            if key_holded("UP"):
+                ud = speed
+
+            if key_holded("DOWN"):
+                ud = -speed
+
+            if key_holded("RIGHT"):
+                cw = speed
+
+            if key_holded("LEFT"):
+                cw = -speed
+
+            self.drone.send_rc_control(lr, fb, ud, cw)
 
     
-    def controller(self):
-        self.speed = 60
-        self.fb, self.lr, self.ud, self.cw = 0, 0, 0, 0
-
-        if GetKey("t"):
-            self.drone.takeoff()
-            self.send_rc_control = True
-        elif GetKey("l"):
-            self.drone.land()
-            self.send_rc_control = False
-
-        if self.in_fly():
-            if GetKey("LSHIFT"):
-                self.speed += 40
-            else:
-                self.speed = 60
-
-            if GetKey("w"):
-                self.fb = self.speed
-
-            if GetKey("a"):
-                self.lr = -self.speed
-
-            if GetKey("s"):
-                self.fb = -self.speed
-
-            if GetKey("d"):
-                self.lr = self.speed
-
-            if GetKey("UP"):
-                self.ud = self.speed
-
-            if GetKey("DOWN"):
-                self.ud = -self.speed
-
-            if GetKey("RIGHT"):
-                self.cw = self.speed
-
-            if GetKey("LEFT"):
-                self.cw = -self.speed
-
-            if self.send_rc_control:
-                self.drone.send_rc_control(self.lr, self.fb, self.ud, self.cw)
-
-
 if __name__ == "__main__":
     Drone().start()
